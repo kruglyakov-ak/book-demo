a8b82989b7e017c479885eb8987bfaa6257f0b03
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-09-08 23:39:31 +0300

    add loading and error templates

--- app/router.js
+++ app/router.js
@@ -1,17 +1,18 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
-Router.map(function() {
-  this.route('books');
-  this.route('Книжный');
-  this.route('speakers');
-  this.route('edit-book');
-  this.route('edit-speaker');
+Router.map(function () {
+  this.route("books");
+  this.route("Книжный");
+  this.route("speakers");
+  this.route("edit-book");
+  this.route("edit-speaker");
+  this.route("404", { path: "*path" });
 });
 
 export default Router;
