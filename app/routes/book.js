0c92ab76afad5e24e38dcb0931f4ffac6bad7019
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-09-14 23:54:34 +0300

    replace create book item in book/index route

--- app/routes/book.js
+++ app/routes/book.js
@@ -1,10 +1,3 @@
 import Route from "@ember/routing/route";
-import { inject as service } from "@ember/service";
 
-export default Route.extend({
-  dataService: service("data"),
-
-  model() {
-    return this.get("dataService").getBooks();
-  },
-});
+export default Route.extend({});
