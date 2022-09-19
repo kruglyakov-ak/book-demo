4a9f775a271f6fff713015fc78a4b916cca073c1
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-09-19 12:20:13 +0300

    comment tags input

--- app/controllers/book/create.js
+++ app/controllers/book/create.js
@@ -10,39 +10,57 @@ export default Controller.extend({
         title: this.get("title"),
         authorName: this.get("authorName"),
         pageCount: this.get("pageCount"),
-        tags: this.get("tags"),
         rate: 25,
         descriptionLink: this.get("descriptionLink"),
+        // tags: this.get("tags"),
         coverURL: this.get("coverURL"),
       });
       this.transitionToRoute("book");
     },
+
     changeTitle(title) {
       this.set("title", title);
     },
+
     changeAuthorName(authorName) {
       this.set("authorName", authorName);
     },
+
     changePageCount(pageCount) {
       this.set("pageCount", pageCount);
     },
+
     changeDescription(descriptionLink) {
       this.set("descriptionLink", descriptionLink);
     },
+
     changeCover(coverURL) {
       this.set("coverURL", coverURL);
     },
+
     resetCover() {
       this.set("coverURL", null);
     },
-    changeTags(tags) {
-      this.set(
-        "tags",
-        tags.split(",").map((tag) => tag.trim())
-      );
-    },
+
     backToBoks() {
       this.transitionToRoute("book");
     },
+
+    // changeTags(tags) {
+    //   this.set(
+    //     "tags",
+    //     tags.split(",").map((tag) => tag.trim())
+    //   );
+
+    // },
+    // selectTag(tag) {
+    //   if (this.get("tags")) {
+    //     this.set("tags", [...this.get("tags"), tag]);
+    //     console.log(this.get("tags"));
+    //   } else {
+    //     this.set("tags", [tag]);
+    //     console.log(this.get("tags"));
+    //   }
+    // },
   },
 });
