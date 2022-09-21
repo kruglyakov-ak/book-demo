42b558d1c90707e648aa2c2f7e6c15df64f02c5e
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-09-22 00:04:52 +0300

    create speaker-form component

--- app/controllers/speaker/create.js
+++ app/controllers/speaker/create.js
@@ -1,24 +1,31 @@
 import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
+import EmberObject from "@ember/object";
 
 export default Controller.extend({
   dataService: service("data"),
-
+  init() {
+    this._super(...arguments);
+    this.set("speaker", EmberObject.create());
+    this.get("speaker").set("name", null);
+    this.get("speaker").set("surname", null);
+    this.get("speaker").set("patronymic", null);
+  },
   actions: {
     async saveSpeaker(evt) {
       evt.preventDefault();
 
       if (evt.submitter.dataset.name === "save") {
         await this.get("dataService").createSpeaker({
-          name: this.get("name"),
-          surname: this.get("surname"),
-          patronymic: this.get("patronymic"),
+          name: this.get("speaker").get("name"),
+          surname: this.get("speaker").get("surname"),
+          patronymic: this.get("speaker").get("patronymic"),
         });
       }
 
-      this.set("name", null);
-      this.set("surname", null);
-      this.set("patronymic", null);
+      this.get("speaker").set("name", null);
+      this.get("speaker").set("surname", null);
+      this.get("speaker").set("patronymic", null);
       this.transitionToRoute("speaker");
     },
 
