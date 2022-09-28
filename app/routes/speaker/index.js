d938d106e4f3852db96d98274216310e190c57f2
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-09-28 22:20:02 +0300

    fetch speakers using ember data

--- app/routes/speaker/index.js
+++ app/routes/speaker/index.js
@@ -9,37 +9,38 @@ export default Route.extend({
     },
   },
   async model() {
-    return {
-      isLoading: true,
-    };
+    // return {
+    //   isLoading: true,
+    // };
+    return this.get("store").findAll("speaker");
   },
 
-  setupController(controller) {
+  setupController() {
     this._super(...arguments);
-    let promise = new Promise(async (resolve, reject) => {
-        try {
-          let speakers = controller.get("search")
-            ? await this.get("dataService").getSpeakers(controller.get("search"))
-            : await this.get("dataService").getSpeakers();
-          resolve(speakers);
-        } catch (e) {
-          reject("Connection failed");
-        }
-    })
-      .then((speakers) => {
-        this.set("controller.model", speakers);
-      })
-      .finally(() => {
-        if (promise === this.get("modelPromise")) {
-          this.set("controller.isLoading", false);
-        }
-      });
+    // let promise = new Promise(async (resolve, reject) => {
+    //     try {
+    //       let speakers = controller.get("search")
+    //         ? await this.get("dataService").getSpeakers(controller.get("search"))
+    //         : await this.get("dataService").getSpeakers();
+    //       resolve(speakers);
+    //     } catch (e) {
+    //       reject("Connection failed");
+    //     }
+    // })
+    //   .then((speakers) => {
+    //     this.set("controller.model", speakers);
+    //   })
+    //   .finally(() => {
+    //     if (promise === this.get("modelPromise")) {
+    //       this.set("controller.isLoading", false);
+    //     }
+    //   });
 
-    this.set("modelPromise", promise);
+    // this.set("modelPromise", promise);
 
-    if (this.get("modelPromise")) {
-      controller.set("isLoading", true);
-    }
+    // if (this.get("modelPromise")) {
+    //   controller.set("isLoading", true);
+    // }
   },
 
   actions: {
