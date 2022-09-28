4fda56df804e5929e308aa51bcf48adc8506c463
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-09-29 00:34:48 +0300

    fix rate style

--- app/components/book-item.js
+++ app/components/book-item.js
@@ -1,4 +1,6 @@
 import Component from "@ember/component";
+import { htmlSafe } from "@ember/template";
+import { computed } from "@ember/object";
 
 export default Component.extend({
   actions: {
@@ -6,4 +8,8 @@ export default Component.extend({
       this.routeByTag(tag);
     },
   },
+
+  rateWidth: computed(function () {
+    return htmlSafe(`width: ${this.get("book.rate")}%;`);
+  }),
 });
