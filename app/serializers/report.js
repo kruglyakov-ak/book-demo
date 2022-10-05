71c3679f32e39ee29b8391f81c850518ca3a4647
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-10-05 04:10:26 +0300

    fix

--- app/serializers/report.js
+++ app/serializers/report.js
@@ -1,19 +1,4 @@
 import ApplicationSerializer from "./application";
-import DS from "ember-data";
 
-export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
-  attrs: {
-    meeting: {
-      serialize: "id",
-      deserialize: "records",
-    },
-    book: {
-      serialize: "id",
-      deserialize: "records",
-    },
-    speaker: {
-      serialize: "id",
-      deserialize: "records",
-    },
-  },
+export default ApplicationSerializer.extend({
 });
