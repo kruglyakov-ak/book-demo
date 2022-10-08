5379baf0431ae23c89142a9380b83943738fa496
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-10-08 16:46:42 +0300

    fix upload cover

--- app/services/data.js
+++ app/services/data.js
@@ -2,49 +2,13 @@ import Service from "@ember/service";
 import ENV from "book-demo/config/environment";
 
 export default Service.extend({
-  async getBooks(search, searchByTags) {
-    const queryParam = search || searchByTags ? "?" : "";
-    const querySearch = search ? `q=${search}` : "";
-    const querySearchByTags = searchByTags ? `tags_like=${searchByTags}` : "";
-    const response = await fetch(
-      `${ENV.backendURL}/books${queryParam}${querySearch}${
-        querySearchByTags ? "&" : ""
-      }${querySearchByTags}`
-    );
-    return await response.json();
-  },
-
-  async getBook(id) {
-    const response = await fetch(`${ENV.backendURL}/books/${id}`);
-    return await response.json();
-  },
-
-  async getSpeaker(id) {
-    const response = await fetch(`${ENV.backendURL}/speakers/${id}`);
-    return await response.json();
-  },
-
-  async getSpeakers(search) {
-    let queryParams = "";
-    if (search) {
-      queryParams = `?q=${search}`;
-    }
-    const response = await fetch(`${ENV.backendURL}/speakers${queryParams}`);
-    return await response.json();
-  },
-
-  async deleteBook(id) {
-    return await fetch(`${ENV.backendURL}/books/${id}`, { method: "DELETE" });
-  },
-
-  async deleteSpeaker(id) {
-    return await fetch(`${ENV.backendURL}/speakers/${id}`, {
-      method: "DELETE",
-    });
-  },
-
-  _uploadBookData(savedBook, uploadData) {
+  uploadBookData(savedBook, uploadData) {
     return new Promise(async (resolve, reject) => {
+
+      if (!uploadData) {
+        resolve();
+      }
+
       uploadData.url = `${ENV.fileUploadURL}`;
       uploadData.url = `${ENV.fileUploadURL}`;
       // uploadData.headers = getOwner(this).lookup('adapter:application').get('headers');
@@ -76,71 +40,6 @@ export default Service.extend({
         .fail((jqXhr, textStatus, errorThrown) => {
           reject(errorThrown);
         });
-    });
-  },
-
-  async createBook(book, uploadData) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const savedBookPromise = await fetch(`${ENV.backendURL}/books`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(book),
-        });
-
-        const savedBook = await savedBookPromise.json();
-
-        if (!uploadData) {
-          resolve();
-        }
-
-        this._uploadBookData(savedBook, uploadData);
-      } catch (e) {
-        reject(e);
-      }
-    });
-  },
-
-  async updateBook(book, uploadData) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const savedBookPromise = await fetch(
-          `${ENV.backendURL}/books/${book.id}`,
-          {
-            method: "PATCH",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(book),
-          }
-        );
-
-        const savedBook = await savedBookPromise.json();
-
-        if (!uploadData) {
-          resolve();
-        }
-
-        this._uploadBookData(savedBook, uploadData);
-      } catch (e) {
-        reject(e);
-      }
-    });
-  },
-
-  async createSpeaker(speaker) {
-    return await fetch(`${ENV.backendURL}/speakers`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(speaker),
-    });
-  },
-
-  async updateSpeaker(speaker) {
-    return await fetch(`${ENV.backendURL}/speakers/${speaker.id}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(speaker),
     });
   },
 });
