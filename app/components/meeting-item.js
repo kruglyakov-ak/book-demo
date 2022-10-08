9d3d4d94dbdcf8139aebb3b17761e3e1a7d015e1
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-10-08 15:20:24 +0300

    fix create report action

--- app/components/meeting-item.js
+++ app/components/meeting-item.js
@@ -1,10 +1,3 @@
 import Component from "@ember/component";
 
-export default Component.extend({
-  actions: {
-    async deleteMeeting(meeting) {
-      await meeting.destroyRecord();
-      await this.get("store").unloadRecord(meeting);
-    },
-  },
-});
+export default Component.extend({});
