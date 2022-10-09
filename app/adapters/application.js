b6fff89824142cdd1163c97e4110b5b3efb739c3
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-10-09 18:22:50 +0300

    set authorization

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,15 +1,23 @@
 import DS from "ember-data";
 import ENV from "book-demo/config/environment";
+import { computed } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default DS.JSONAPIAdapter.extend({
   host: ENV.backendURL,
+  session: service(),
 
-  init() {
-    this._super(...arguments);
-    this.set("headers", {
+  headers: computed(function () {
+    let resultHeaders = {
       "Content-Type": "application/json",
-    });
-  },
+    };
+
+    if (this.get("session.isAuthenticated")) {
+      resultHeaders[
+        "Authorization"
+      ] = `Bearer ${this.session.data.authenticated.token}`;
+    }
+  }).volatile(),
 
   buildURL(modelName, id, snapshot, requestType) {
     let url = this._super(...arguments);
